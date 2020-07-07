cfce63d68e2949b29ffa538e5ba4bef7d50561a1
cairz <cairz@ffcs.cn>
2020-07-07 11:35:02 +0800

    制卡

--- src/main/java/com/crongze/util/QQNickUtil.java
+++ src/main/java/com/crongze/util/QQNickUtil.java
@@ -1,6 +1,19 @@
 package com.crongze.util;
 
-public class QQNickUtil {
-    private static final String url = "https://r.qzone.qq.com/fcg-bin/cgi_get_score.fcg?mask=7&uins=";
+import org.springframework.web.client.RestTemplate;
 
+public class QQNickUtil {
+    private static final String BASE_URL = "https://r.qzone.qq.com/fcg-bin/cgi_get_score.fcg?mask=7&uins=";
+    private static RestTemplate restTemplate = new RestTemplate();
+
+    public static String getQQNick(long qq){
+        String url = BASE_URL + qq;
+        String response = restTemplate.getForObject(url, String.class);
+        if(response == null){
+            return "匿名";
+        }
+        response.replace("", "");
+        response.replace("", "");
+        return "匿名";
+    }
 }
